ab642e692787ab62a09fc7b0891c661b3dca6f71
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-11-04 18:31:57 -0300

    Creado ruteo individual a componente de proyecto

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
 import { ProjectMiniComponent } from './project-mini/project-mini.component';
+import { ProjectComponent } from './project/project.component';
 
 
 @NgModule({
   declarations: [
     ProjectsComponent,
-    ProjectMiniComponent
+    ProjectMiniComponent,
+    ProjectComponent
   ],
   imports: [
     CommonModule,
