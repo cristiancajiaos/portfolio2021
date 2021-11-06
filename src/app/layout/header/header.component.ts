cbcb6dcecca1b22726dbaa0aad60e0ac2f81f846
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-11-06 12:39:52 -0300

    Definida apariencia de header para pantallas menores a lg, eliminado efecto opacidad por problemas de compatibilidad con navbar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isMenuCollapsed = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleCollapse(): void {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
   }
 
 }
